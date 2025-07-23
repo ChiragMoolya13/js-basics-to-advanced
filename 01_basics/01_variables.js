efafdc11162ddbf8e0054e64721156b530b27083
Chirag Moolya <chiragmoolya13@gmail.com>
2025-07-23 21:13:42 +0530

    made some changes today

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -12,3 +12,30 @@ console.table([UserID,accountName,accountMail,accountLocation,accountState]); //
 /*
 Going forward stop using var as a variable type as it is outdated and has some issues with block scope and functional scope.
 */
+
+/*
+Conceptual Questions
+1. What are the key differences between let, const, and var in terms of:
+    -Reassignability
+    -Scope
+    -Hoisting
+2. What would happen if you declare the same variable name twice using var vs let?
+3. Can a const variable hold an object or array? If yes, can you modify its contents?
+ 
+*/
+
+/*
+Practice Questions for the topic:
+1. Declare a const variable PI with the value 3.14. Try to reassign it. What happens?
+2. Declare a let variable score with value 10. Reassign it to 20. Print both values.
+3. Write a block of code with an if statement where you declare a var inside it. Try to access that variable outside the block. Does it work?
+4. Repeat question 3 but use let instead of var. Does it work now?
+5. Declare a const object person = { name: "Alex" }. Change the name to "Sam". Does it work? Why?
+6. Try declaring the same variable x twice using var. Then do the same using let. Observe the error. 
+*/
+
+/*
+Mini Challenge
+Problem:
+Write a function createCounter() that uses let to maintain a count variable inside. Every time the function is called, it should increment and print the counter. Try doing the same with var and observe what changes.
+*/
