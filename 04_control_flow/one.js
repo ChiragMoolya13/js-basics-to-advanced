8de74336d453ceb003f39d768a5ed13a2442b519
Chirag Moolya <chiragmoolya13@gmail.com>
2026-02-09 11:38:27 +0530

    completed Control Flow & Loops

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+//if
+// const isUserLoggedin = true
+// const temperature = 41
+// if(temperature === 41){
+//     // console.log("less then 50");
+// }else{
+//     // console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+
+//<, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+// if(score>100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// const balance = 1000
+
+// if(balance>500) console.log("test"),console.log("test2"); dont do this (wrong pratice)
+
+// if(balance > 500){
+//     console.log("less than");
+// }else if(balance<750){
+//     console.log("less than 750");
+// }else if(balance<900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard ){
+    console.log("Allowed to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromGoogle){
+    console.log("User logged in");
+}
